350c89bd3683981dce4f0ab3b4a85ee5d33e53a9
Justin <justin@justintime.software>
2025-04-09 15:03:11 -0400

    feat: initial system

---
+++ Haggard.Engine/HaggardGameEngine.cs
@@ -0,0 +1,23 @@
+﻿namespace Haggard.Engine;
+
+public class HaggardGameEngine : IGameEngine
+{
+    public event Action<float>? Tick;
+    public event Action<float>? Render;
+
+    public HaggardGameEngine()
+    {
+        
+    }
+
+    public async Task RunAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Tick?.Invoke(10);
+            await Task.Delay(TimeSpan.FromSeconds(1),CancellationToken.None);
+        }
+    }
+
+
+}
---
+++ Haggard.Engine/IGameEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace Haggard.Engine;
+
+/// <summary>
+/// A game engine is used to initialize and create the code necessary for the first startup procedures.
+/// </summary>
+public interface IGameEngine
+{
+    public event Action<float> Tick;
+    public event Action<float> Render;
+}
