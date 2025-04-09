cb4c3f07223733fd9c3bcb54cab6f391b564b62d
Justin <justin@justintime.software>
2025-04-09 15:12:38 -0400

    chore: add unit tests, rider files

---
+++ Haggard.Engine.Tests/Haggard.Engine.Tests/EngineTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Haggard.Engine.Tests;
+
+public class EngineTests
+{
+    [Fact]
+    public async Task EngineTicksEvent()
+    {
+        var cancellationToken = new CancellationTokenSource();
+        var engine = new HaggardGameEngine();
+        var tick = 0;
+        engine.Tick += (_) => tick++;
+
+        await Task.WhenAll(engine.RunAsync(cancellationToken.Token), CancelTask());
+        
+        Assert.NotEqual(0, tick);
+        return;
+
+        async Task CancelTask()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), CancellationToken.None);
+            await cancellationToken.CancelAsync();
+        }
+    }
+}
