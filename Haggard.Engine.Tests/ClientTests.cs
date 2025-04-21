25f2131103ebf3b550f15e8fe2ac5f351ce19e1b
Justin <justin@justintime.software>
2025-04-21 17:01:02 -0400

    fix: remove test suite nesting

---
+++ Haggard.Engine.Tests/ClientTests.cs
@@ -0,0 +1,20 @@
+﻿using Haggard.Engine.Client.Windowing;
+using Haggard.Engine.Tests.Extensions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+namespace Haggard.Engine.Tests;
+
+public class ClientTests
+{
+    [Fact]
+    public async Task WindowGetsCreated()
+    {
+        var gameEngine = Utils.CreateBasicEngine();
+        var windowManager = new HaggardWindowManager(NullLogger<HaggardWindowManager>.Instance, gameEngine);
+        Assert.Null(windowManager.CurrentWindow);
+        await gameEngine.StartBackground();
+        Assert.NotNull(windowManager.CurrentWindow);
+        Assert.False(windowManager.CurrentWindow.IsClosing);
+    }
+}
