77d5d4d30f045b3c000b5e52426df8723b29364b
Justin <justin@justin.directory>
2025-09-28 17:14:47 -0400

    feat(samples): initial sample

---
+++ Haggard.Engine.Samples/Program.cs
@@ -0,0 +1,14 @@
+﻿using Haggard.Engine;
+using Haggard.Engine.Client.Graphics.Vulkan;
+using Haggard.Engine.Client.Windowing;
+using Microsoft.Extensions.Logging.Abstractions;
+
+var gameEngine = new HaggardGameEngine(NullLogger<HaggardGameEngine>.Instance);
+var windowing = new HaggardWindowManager(NullLogger<HaggardWindowManager>.Instance, gameEngine);
+var rendering = new VulkanRenderingSystem(
+    NullLogger<VulkanRenderingSystem>.Instance,
+    gameEngine,
+    windowing
+);
+
+await gameEngine.StartAsync(CancellationToken.None);
